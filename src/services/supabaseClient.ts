7ce9272ba2b0073825c9fad4328471f78e526c24
Bruno Dorigon <bdorigon@gmail.com>
2025-05-13 07:34:05 -0300

    build .env in process with database

--- src/services/supabaseClient.ts
+++ src/services/supabaseClient.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://mkjmyfahlmtkfoailair.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ram15ZmFobG10a2ZvYWlsYWlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYzMTEzNTcsImV4cCI6MjA2MTg4NzM1N30.e7V4ol9d4IHmswyoMHCDoqD2CXE6kvLyYg0EgqTnjCI";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
